6a46d385719353268d5744a08ff4aea5ad365364
9113358 <nd9113358@gmail.com>
2026-03-28 08:49:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,27 @@
 const CACHE = "mini-app-v1";
 
-self.addEventListener("install", e => {
+self.addEventListener("install", e=>{
  e.waitUntil(
-   caches.open(CACHE).then(cache => {
+   caches.open(CACHE).then(cache=>{
      return cache.addAll([
        "./",
        "./index.html",
        "./style.css",
        "./script.js",
-       "./manifest.json"
+       "./manifest.json",
+       "./Cake.jpg",
+       "./GiftBox.png",
+       "./HappyBirthdayImage.png",
+       "./music.mp3",
+       "./icon-192.png",
+       "./icon-512.png"
      ]);
    })
  );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", e=>{
  e.respondWith(
-   caches.match(e.request).then(res => res || fetch(e.request))
+   caches.match(e.request).then(res=>res||fetch(e.request))
  );
 });
